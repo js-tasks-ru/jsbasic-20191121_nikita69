b7068afd3385044c00ccf3b7317a8beececcce3f
Solarius <nikitan9273@gmail.com>
2019-12-05 18:32:57 +0300

    complete 3-module

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -4,4 +4,14 @@
  * @returns {{min:number, max:number}}  объект
  */
 function getMinMax(str) {
+  //let ar = str.split(',').join(' ').split(' '); переделал с использованием regexp
+  let ar = str.split(/[,\s]+/g);
+  // хотя проще и правильнее было бы использовать метод str.match(regexp) для возврата в виде массива только числа, т.к. 
+  // между цифрами могут быть любые символы не только , и ' '  и тогда текущий код не отработает как надо, 
+  // но с учетом текущих проверочных тестов оставлю как есть 
+  let ar2 = ar.map(el => parseFloat(el)).filter(el => !isNaN(el));
+  return {
+    min: Math.min(...ar2),
+    max: Math.max(...ar2),
+  };
 }
