dc5f1799bb01b8b19f612474b4c0063993530a7d
Solarius <nikitan9273@gmail.com>
2020-02-12 20:00:55 +0300

    Complete module 5

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -3,4 +3,33 @@
  * @param {Element} table
  */
 function highlight(table) {
+  for (let el of table.rows) {
+    if (el.cells[3].hasAttribute('data-available')) {
+      let tf = el.cells[3].getAttribute('data-available');
+      if (tf === 'true') {
+        el.classList.add('available');
+      }
+      else if (tf === 'false') {
+        el.classList.add('unavailable');
+      }
+    } 
+    else {
+      el.setAttribute('hidden', '');
+    }
+  }
+  for (let el of table.rows) {
+    let age = el.cells[1];
+    if (parseInt(age.textContent, 10) < 18) {
+      el.style.textDecoration = 'line-through';
+    }
+  }
+  for (let el of table.rows) {
+    let gndr = el.cells[2];
+    if (gndr.textContent === 'm') {
+      el.classList.add('male');
+    }
+    else if (gndr.textContent === 'f') { 
+      el.classList.add('female');
+    }
+  }
 }
--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -14,16 +14,54 @@
  * @constructor
  */
 function SortableTable(items) {
+  
   /**
    * @property {Element} - обязательно свойство, которое ссылается на элемент <table>
    */
   this.el = document.createElement('table');
 
+  let thead = document.createElement('thead');
+  let tbody = document.createElement('tbody');
+
+  this.el.prepend(thead);
+  this.el.append(tbody);
+
+  let newitems = Object.assign([], items);
+
+  thead.innerHTML = newitems.slice(0, 1)
+   .map((item) => {
+     let newrow = '';
+     for (let newkey in item) {
+       newkey = newkey.replace(/^\w/, c => c.toUpperCase());
+       newrow += `<td>${newkey}</td>`;
+     }
+     return `<tr>${newrow}</tr>`;
+   }).join('');
+  function tBody() {
+    tbody.innerHTML = items.map((item) => {
+      let row = '';
+      for (let key in item) {
+        row += `<td>${item[key]}</td>`;
+      }
+      return `<tr>${row}</tr>`;
+    }).join('');
+  }
   /**
    * Метод выполняет сортировку таблицы
    * @param {number} column - номер колонки, по которой
    * нужно выполнить сортировку (отсчет начинается от 0)
    * @param {boolean} desc - признак того, что сортировка должна идти в обратном порядке
    */
-  this.sort = (column, desc = false) => {};
+  this.sort = (column, desc = false) => {
+    items = items.sort((a, b) => { 
+      let rowA = Object.values(a)[column];
+      let rowB = Object.values(b)[column];
+      if (desc) {
+        return rowA < rowB ? 1 : -1;
+      }
+      return rowA > rowB ? 1 : -1;
+    });
+    tBody();
+  };
+  tBody();
 }
